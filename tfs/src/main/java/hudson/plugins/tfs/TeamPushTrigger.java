2094fa346eb1a48b9197d0237667f6bac7f02f0f
Stoyan Todorov Todorov <sto@milestone.dk>
2018-10-26 18:01:13 +0300

    delete comment

--- tfs/src/main/java/hudson/plugins/tfs/TeamPushTrigger.java
+++ tfs/src/main/java/hudson/plugins/tfs/TeamPushTrigger.java
@@ -164,7 +164,6 @@ public class TeamPushTrigger extends Trigger<Job<?, ?>> {
                     cause = new TeamPushCause(pushedBy, getJobContext());
                 }
                 final int quietPeriod = p.getQuietPeriod();
-                // final CauseAction causeAction = new CauseAction(cause);
                 final ParametersAction parameterActionTest = new ParametersAction(new StringParameterValue("BRANCHNAMETEST", "branchNameTest"));
                 final Action[] actionArray = ActionHelper.create(actions, parameterActionTest);
                 final QueueTaskFuture<?> queueTaskFuture = p.scheduleBuild2(quietPeriod, actionArray);
