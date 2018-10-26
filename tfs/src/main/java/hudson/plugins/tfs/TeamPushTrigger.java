4c2ccfd82e825bb558867c57477de8a161166252
Stoyan Todorov Todorov <sto@milestone.dk>
2018-10-26 17:52:22 +0300

    try diff param setting

--- tfs/src/main/java/hudson/plugins/tfs/TeamPushTrigger.java
+++ tfs/src/main/java/hudson/plugins/tfs/TeamPushTrigger.java
@@ -165,7 +165,8 @@ public class TeamPushTrigger extends Trigger<Job<?, ?>> {
                 }
                 final int quietPeriod = p.getQuietPeriod();
                 final CauseAction causeAction = new CauseAction(cause);
-                final Action[] actionArray = ActionHelper.create(actions, causeAction);
+                final parameterActionTest = new ParametersAction(new StringParameterValue("BRANCHNAMETEST", "branchNameTest");
+                final Action[] actionArray = ActionHelper.create(actions, parameterActionTest);
                 final QueueTaskFuture<?> queueTaskFuture = p.scheduleBuild2(quietPeriod, actionArray);
                 if (queueTaskFuture != null) {
                     LOGGER.info(changesDetected + "Triggering " + name);
