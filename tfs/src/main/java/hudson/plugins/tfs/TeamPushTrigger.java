57d7d42db85cd074260297f15c586784f89a9d75
Stoyan Todorov Todorov <sto@milestone.dk>
2018-10-29 09:31:17 +0200

    fix imports

--- tfs/src/main/java/hudson/plugins/tfs/TeamPushTrigger.java
+++ tfs/src/main/java/hudson/plugins/tfs/TeamPushTrigger.java
@@ -32,6 +32,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
